5e272d0274cc3fb9d81e077fb6f848fc63b2ecde
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-06-27 18:26:08 +0530

    Formatted comments.

--- TODO.md
+++ TODO.md
@@ -1,10 +1,11 @@
 **TODO**
 
-- [ ] When request comes with flower name redirect to that flower page.
-- [ ] Create guest form.
 
 **DONE**
 
+- [x] Format comments.
+- [x] When request comes with flower name redirect to that flower page.
+- [x] Create guest form.
 - [x] Style the pages.
 - [x] Create static html with flower names.
 - [x] Create html for each flower.
--- src/guestBookHandler.js
+++ src/guestBookHandler.js
@@ -2,24 +2,34 @@ const fs = require('fs');
 
 const storeComments = (path, content) => {
   fs.writeFileSync(path, JSON.stringify(content), 'utf-8');
-}
+};
+
+const formatComments = (comments) => {
+  return comments.map(record => {
+    const { name, comment, date } = record;
+    return [name, comment, date].join('  ');
+  }).join('\n\n');
+};
 
 const commentsHandler = (request, response) => {
   const { comments, uri, queryParams } = request;
   const { name, comment } = queryParams;
   const date = new Date();
 
+  response.setHeader('conetent-type', 'text/plain');
+
   comments.push({ name, date, comment });
   storeComments(request.commentsPath, comments);
-  response.send(`${name} comment has been stored`);
+  response.send(formatComments(comments));
 
   return true;
-}
+};
 
 const guestBookHandler = (request, response) => {
   const { uri } = request;
+
   if (uri === '/guestbook') {
-    commentsHandler(request, response);
+    return commentsHandler(request, response);
   }
 
   return false;
